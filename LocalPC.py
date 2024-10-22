90fd64b0958d8d12ec61ad16f94bb50a252d28b5
0neShot <92669413+0neShot@users.noreply.github.com>
2024-10-22 22:08:10 +0200

    Update LocalPC.py

--- LocalPC.py
+++ LocalPC.py
@@ -1,33 +1,64 @@
 from pydualsense import pydualsense
-import socket
+import socket, logging
+from config import Config
 from time import sleep
+import logging
 
+connected = False
+client_socket = 0
 
-# Set up the client socket
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(('172.20.10.8', 80))  # Connect to the server
+def initConnection():
+    #Set up the client socket
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.settimeout(5)
+    try:
+        # Connect to the server
+        client_socket.connect(('192.168.137.215', 80))
+        logging.info("Connected to robot successfully")
+        connected = True
+    except socket.timeout:
+            logging.warning("Failed to connect to robot")
 
-def button_pressed(cmd):
-    #print(f"{cmd} button pressed")
-    client_socket.sendall(cmd.encode('utf-8'))
+def sendCommand(action, paramater = None):
+    message = str(action)
+    if paramater is not None:
+        message += ";" + str(paramater)
+    if not connected:
+        logging.warning(f"Failed to send '{message}' because bot is not connected")
+        return
+    logging.info(f'Sending "{message}"')
+    #client_socket.send(message.encode())
 
-def button_released(cmd):
-    #print(f"{cmd} button released")
-    client_socket.sendall(cmd.encode('utf-8'))
+def joystickChanged(stateX, stateY):
+    print(f"{stateX} X-Value")
+    sendCommand(Config.SPEED, stateX)
 
+def trianglePressed(state):
+    print(f"Triangle: {state}")
+    sendCommand(Config.STOPALL)
+    
+def r2Changed(state):
+    print(f"R2: {state}")
+    
 def main():
     # Initialize the DualSense controller
     ds = pydualsense()
     ds.init()
-
+    ds.light.setColorI(255,0,0) # set touchpad color to red
+    
+    # Initialize Connection
+    initConnection()
+    ds.light.setColorI(0,128,0) # set touchpad color to green
+    
     # Set up event handlers for buttons
-    ds.cross_pressed += lambda state: button_pressed("back") if state else button_released("stop")
-    ds.circle_pressed += lambda state: button_pressed("right") if state else button_released("stop")
-    ds.square_pressed += lambda state: button_pressed("left") if state else button_released("stop")
-    ds.triangle_pressed += lambda state: button_pressed("forward") if state else button_released("stop")
+    ds.triangle_pressed += trianglePressed
+    ds.left_joystick_changed += joystickChanged
+    ds.r2_changed += r2Changed
+    print(ds.state.LX)
 
     try:
         print("Press any button to see the output...")
+        print(ds.state.LX)
         while True:
             pass  # Keep the script running to listen for events
     except KeyboardInterrupt:
@@ -37,4 +68,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
