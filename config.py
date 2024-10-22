072abce817e9d5d14005e4d14a804058e6f44ba4
0neShot <92669413+0neShot@users.noreply.github.com>
2024-10-22 22:07:15 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,19 @@
+class Config:
+    "Constants for Communication. Do not change!"
+    SPEED = 1
+    ANGLE = 2
+    STOPALL = 3
+    
+    "Constants for Robot"
+    TRESHOLD = 5000
+    ANALOGSENSORPIN = 28
+    ONBORDLEDPIN = 25
+    
+    "Constants for PID"
+    KP = 1.0
+    KI = 0.1
+    KD = 0.05
+    
+    "Wifi Connection Settings"
+    SSID = 'DESKTOP-12QN0M'
+    PASSWORD = '6d]9129K'
